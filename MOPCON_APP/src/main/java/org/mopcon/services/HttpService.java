7505ed5126fce5788b2967f746d3f6b76b10e3c2
Chuck-MacBook <shen0816@gmail.com>
2013-08-29 20:08:29 +0800

    create httpservice

---
+++ MOPCON_APP/src/main/java/org/mopcon/services/HttpService.java
@@ -0,0 +1,14 @@
+package org.mopcon.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by chuck on 13/8/29.
+ */
+public class HttpService extends Service {
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
