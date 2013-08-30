48dad9953dfd279881a8124e4fb656e4079701a2
Chuck-MacBook <shen0816@gmail.com>
2013-08-30 23:25:16 +0800

    add Key

--- MOPCON_APP/src/main/java/org/mopcon/model/Session.java
+++ MOPCON_APP/src/main/java/org/mopcon/model/Session.java
@@ -4,6 +4,16 @@ package org.mopcon.model;
  * Created by chuck on 13/8/29.
  */
 public class Session {
+    public class Key{
+        public static final String id = "id";
+        public static final String name = "name";
+        public static final String speaker = "speaker";
+        public static final String speaker_bio = "speaker_bio";
+        public static final String keywork = "keywork";
+        public static final String loc = "loc";
+        public static final String start_time = "start_time";
+        public static final String end_time = "end_time";
+    }
     public int id;
     public String name;
     public String speaker;
